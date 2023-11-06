33740b09aef7627a4bc23ad958977fdabcc65902
kong-tae-hyeon-khu <kong3047@khu.ac.kr>
2023-11-06 23:48:14 +0900

    Constructing Components

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
 import SignUpForm from "./pages/SignUpForm";
 import SignInForm from "./pages/SignInForm";
-
+import Main from "./pages/Main";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+            <Route path ="/" element={<Main />}></Route>
           <Route path = "/signup" element ={<SignUpForm/>}></Route>
           <Route path = "/signin" element ={<SignInForm/>}></Route>
         </Routes>
